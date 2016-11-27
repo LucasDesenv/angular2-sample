ceaa2d06207f7f71b9e047954dc4c712f591df04
Lucas de Souza <lde.souza@yahoo.com>
2016-11-26 22:08:04 -0200

    Encapsulando nosso css por componente.

--- client/app/foto/foto.component.ts
+++ client/app/foto/foto.component.ts
@@ -2,7 +2,8 @@ import {Component, Input} from '@angular/core';
 @Component({
     moduleId : module.id,
     selector:'foto',
-    templateUrl:'./foto.component.html'
+    templateUrl:'./foto.component.html',
+    styleUrls: ['./foto.component.css']
 })
 export class FotoComponent {
     //O @Input() serve para informar que essa propriedade aceita receber valores.
